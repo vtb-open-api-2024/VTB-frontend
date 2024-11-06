f3053bae26ba4d63bd1a4dd1558836d068960ab0
Polina Tsukanova <103593628+polexka@users.noreply.github.com>
2024-11-06 23:57:10 +0300

    Revert "Dockerfile"

--- src/components/layout/LayoutComponent.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-export const LayoutComponent = () => {
-  return (
-    <div className="fullWindow">
-      <Outlet />
-    </div>
-  );
-};
--- src/func-pages/history-page/history-element/HistoryElement.tsx
+++
@@ -1,5 +0,0 @@
-
-
-export const HistoryElement = () => {
-  return <li>sdds</li>;
-};
